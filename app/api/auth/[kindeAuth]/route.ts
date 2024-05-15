ba28535b03bd96d808bc16f4be9e0d1fd3248e62
yokeshsusaimanickam <syokesh197@gmail.com>
2024-05-15 20:40:24 +0530

    Authentication and animated landing page section

---
+++ app/api/auth/[kindeAuth]/route.ts
@@ -0,0 +1,5 @@
+// [kindeAuth] [] is next.js naming convention for dynamic route
+
+
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+export const GET = handleAuth();
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -59,18 +59,24 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+       "fade-in":{
+        from:{
+          opacity:"0",
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+        to:{
+          opacity:"1"
+        }
+       },
+        marquee:{
+          '100%':{
+            transform:'translateY(-50%)'
+          }
+        }
       },
+      /* we set animation marquee duration in Reviews */
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        "marquee":"marquee var(--marquee-duration) linear infinite",
+        "fade-in":"fade-in 0.5s linear forwards"
       },
     },
   },
